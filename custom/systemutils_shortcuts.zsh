97dfa4114ea1e895a537497627fd56e51f839668
Seth Derrick <seth@sethderrick.tech>
2024-05-18 10:43:23 -0500

    Update systemutils_shortcuts.zsh

--- custom/systemutils_shortcuts.zsh
+++ custom/systemutils_shortcuts.zsh
@@ -1,5 +1,7 @@
+alias free="vm_state | grep free" # display free and used memory
 alias logs="tail -f /var/log/system.log" # tail system logs
 alias pkill="pkill -f" # kill a process by name
+alias reload="source ~/.zshrc"
 alias shutdown="sudo showdown -h now" # shutdown computer
 alias top="top -o cpu" # Display processes sorted by cpu Usage
 alias update="brew update && brew upgrade"
