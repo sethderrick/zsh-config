7ad58b07132fc624d9c874f42f047a05aa48e275
Seth Derrick <seth@sethderrick.tech>
2025-01-10 17:23:33 -0600

    change systemutils_shortcut to use pacman

--- custom/systemutils_shortcuts.zsh
+++ custom/systemutils_shortcuts.zsh
@@ -1,8 +1,8 @@
-alias free="vm_state | grep free" # display free and used memory
-alias logs="tail -f /var/log/system.log" # tail system logs
-alias pkill="pkill -f" # kill a process by name
+alias free="free -h" # Manjaro has free command built-in
+alias logs="journalctl -f" # Use journalctl instead of system.log
+alias pkill="pkill -f"
 alias reload="source ~/.zshrc"
-alias shutdown="sudo showdown -h now" # shutdown computer
-alias top="top -o cpu" # Display processes sorted by cpu Usage
-alias update="brew update && brew upgrade"
+alias shutdown="shutdown -h now"
+alias top="top -o %CPU"
+alias update="sudo pacman -Syu" # Update using pacman
 
